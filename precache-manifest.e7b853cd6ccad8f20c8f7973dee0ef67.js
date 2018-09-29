d88a4a4e8a6c253d3423a05037af05928f75fdb4
amalcaraz <amalcaraz89@gmail.com>
2018-09-29 18:41:05 +0200

    83b6b1e

---
+++ precache-manifest.e7b853cd6ccad8f20c8f7973dee0ef67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "352d85eeec5fc740a764",
+    "url": "/js/chunk-vendors.352d85ee.js"
+  },
+  {
+    "revision": "eeb9ddc9cfade33ad80a",
+    "url": "/js/app.eeb9ddc9.js"
+  },
+  {
+    "revision": "f3c0bdcc70cc007f5c909d41c1341286",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76a9b07f925b41caa5d84e19f0c050f3",
+    "url": "/img/logo_nuls.76a9b07f.png"
+  },
+  {
+    "revision": "352d85eeec5fc740a764",
+    "url": "/css/chunk-vendors.268a1133.css"
+  },
+  {
+    "revision": "eeb9ddc9cfade33ad80a",
+    "url": "/css/app.b8ef04f6.css"
+  },
+  {
+    "revision": "653d077300a12f09a69caeea7a8947f8",
+    "url": "/browserconfig.xml"
+  }
+];
